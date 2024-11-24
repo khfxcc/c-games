3be46faf5f5ff98f87af065fe37ea38711dd9ce5
khfxcc <khfxcc666@gmail.com>
2024-11-24 19:40:42 +0800

    update

--- 五子棋-unfinished.c
+++ 五子棋-unfinished.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define MAX_ROW 16
-#define MAX_COL 16
+#define MAX_ROW 15
+#define MAX_COL 15
 #define BLACK 1
 #define WHITE -1
 
 void draw_map(char [][]);
 void game_start();
 void initialize(char[][]);
+int check_map();
 
 int main()
 {
+    system("title 五子棋");
     while(1)
     {
-        printf("Press Enter to begin,other to exit.\n");
+        printf("Press 'Block' to begin,'Esc' to exit.\n");
         if (getchar() == '\n')
         {
             game_start();
@@ -33,8 +35,20 @@ void initialize(char map[][])
     {
         for (int j = 0; j < MAX_COL*2; j++)
         {
-            if (i == 0&&j == 0) printf("┌"); 
-            else if (i == MAX_ROW*2-1&&j == 0) printf("└");
+            if (i == 0&&j == 0) map[i][j] == "┏"; 
+            else if (i == MAX_ROW*2-1&&j == 0) map[i][j] = "┗";
+            else if (i == 0&&j == MAX_COL*2-1) map[i][j] = "┓";
+            else if (i == MAX_ROW*2-1&&j == MAX_COL*2-1) map[i][j] = "┛";
+            else if (i == 0&&j%2 == 0) map[i][j] = "┯";
+            else if (i == MAX_ROW*2-1&&j%2 == 0) map[i][j] = "┷";
+            else if (i%2 == 0&&j == 0) map[i][j] = "┠";
+            else if (i%2 == 0&&j == MAX_COL*2-1) map[i][j] = "┨";
+            else if ((i == 0||i == MAX_ROW*2-1)&&j%2 == 1) map[i][j] = "━";
+            else if ((j==0||j==MAX_COL*2-1)&&i%2==1) map[i][j] = "┃";
+            else if (i%2==0&&j%2==0) map[i][j] = "┼";
+            else if (i%2==0&&j%2==1) map[i][j] = "—";
+            else if (i%2==1&&j%2==0) map[i][j] = "│";
+            else map[i][j] = " ";
         }
     }
 }
